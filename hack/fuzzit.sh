81552daeeebced56212e703d349a5497d343fcc7
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-09-08 00:29:09 -0400

    Pull depends with go mod

--- hack/fuzzit.sh
+++ hack/fuzzit.sh
@@ -27,9 +27,10 @@ ROOT=.
 TYPE=$1
 
 # Setup
-export GO111MODULE="off"
+# export GO111MODULE="off"
 go get -u github.com/dvyukov/go-fuzz/go-fuzz github.com/dvyukov/go-fuzz/go-fuzz-build
-go get -d -v -u ./...
+# go get -d -v -u ./...
+go mod vendor
 if [ ! -f fuzzit ]; then
     wget -q -O fuzzit https://github.com/fuzzitdev/fuzzit/releases/download/v2.4.29/fuzzit_Linux_x86_64
     chmod a+x fuzzit
